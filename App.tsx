05b8002fb88b198a9a0fa7f8ead42a1ca459171e
Elias M Olivares <elias.manque.o@gmail.com>
2024-04-18 20:32:43 -0400

    Preparacion de proyecto para instalar axios

--- App.tsx
+++ App.tsx
@@ -4,20 +4,27 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
 import ForgotPass from './screens/ForgotPass'; 
 import Register from './screens/Register';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const Stack = createNativeStackNavigator();
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000',
+  cache: new InMemoryCache()
+
+})
+
 export default function App() {
   return (
+    <ApolloProvider client={client}>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component = {Login}/>
         <Stack.Screen name="ForgotPass" component={ForgotPass}/>
         <Stack.Screen name="Register" component={Register}/>
       </Stack.Navigator>
-
     </NavigationContainer>
-      
+    </ApolloProvider>
   );
 }
 
